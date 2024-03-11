b67795b4a1f407c0dba348dd5ea587c6193f77de
Eliúde Vemba <sweeteliude@gmail.com>
2024-03-11 16:14:32 +0100

    feat(auth): funcionalidade e lógica para registro de utilizador

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import {
   LoginRequestDTO,
   LoginResponseDTO,
@@ -7,7 +8,8 @@ import {
 } from './dto';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -25,9 +27,54 @@ export class AuthService {
   }
 
   async register(dto: RegisterRequestDTO): Promise<RegisterResponseDTO> {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+          dateOfBirth: dto.dateOfBirth,
+          gender: dto.gender,
+          province: dto.province,
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new BadRequestException('Invalid credentials');
+        }
+      }
+    }
+  }
+
+  private async signToken(
+    id: number,
+    email: string,
+  ): Promise<{ access_token: string; refresh_token: string }> {
+    const payload = {
+      sub: id,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '1000m',
+      secret: secret,
+    });
+
+    const refreshToken = await this.jwt.signAsync(payload, {
+      expiresIn: '5000m',
+      secret: secret,
+    });
+
     return {
-      access_token: dto.email,
-      refresh_token: dto.firstName,
+      access_token: token,
+      refresh_token: refreshToken,
     };
   }
 }
