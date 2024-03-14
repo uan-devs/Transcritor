ca39e11e3f7ef9b187ed2363afbe30476130a97a
Eliúde Vemba <sweeteliude@gmail.com>
2024-03-14 06:15:12 +0100

    chore(config): Definição das urls da api

--- clients/mobile/lib/src/common/constants/urls.dart
+++ clients/mobile/lib/src/common/constants/urls.dart
@@ -1,1 +1,10 @@
-const String baseURL = 'localhost:3000/api/v1';
+sealed class UrlsConstants {
+  static const String baseURL = 'http://10.0.2.2:3000';
+  static const String registerUrl = '/auth/register';
+  static const String loginUrl = '/auth/login';
+  static const String refreshTokenUrl = '/auth/refresh-token';
+  static const String userInfoUrl = '/users/me';
+  static const String editUserInfoUrl = '/users/me';
+  static const String changeUserPassword = '/users/change-password';
+  static const String updateUserPhoto = '/users/upload-image';
+}
